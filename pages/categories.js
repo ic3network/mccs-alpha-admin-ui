c5a584a302c4d281867e88c142888351069ebfd9
Geoff Turk <geoffturk27@gmail.com>
2020-04-20 23:58:05 +0200

    Cleanup POST categories

--- pages/categories.js
+++ pages/categories.js
@@ -1,30 +1,23 @@
 import { useState } from 'react'
-import useSWR, { mutate } from 'swr'
+import useSWR from 'swr'
 import fetcher from '../libs/fetch'
-import fetch from 'isomorphic-unfetch'
 
-function PostCategories() {
-    return <InputCategories />
-}
-
-function InputCategories() {
-    const defaultState = {
-        name: ''
-    }
+export default function Categories() {
+    const { data, error } = useSWR('http://localhost:8080/api/v1/categories', fetcher)
+    const defaultState = { category: '' }
     const [state, setState] = useState(defaultState)
 
     async function handleSubmit(event) {
         event.preventDefault()
-        mutate('http://localhost:8080/api/v1/admin/categories', await fetch('http://localhost:8080/api/v1/admin/categories', {
+        await fetcher('http://localhost:8080/api/v1/admin/categories', {
             method: 'POST',
-            // mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODcyOTMzMDEsInVzZXJJRCI6IjVlOWFkMDIwMDA0NTkyZDRlZDIzNWY5NiIsImFkbWluIjp0cnVlfQ.pvtdJ440pe2KqbSMotxGnxvnfdgNahpFezBiUfd-gIcEDEVxBSZkYjNOaNIV2JLyBkb7217cZYdhRxoVQJAawN1gVjL2TFquZD1SDkYkUzuO9lvBUYdrrG7gN6ccagPdcgqcak1tHD3aP99HVtfYwhGIh1ZkObjpC5ddTBKLCh5LygMFzIvpAsKnkkqdSXCIUbLdr8GdMB3y41giWewpx8aCExSVpo26HDVmdkKCsNHMn-Zv7I4kz9Pw1gcSrqDwtVa46fjvV5nOChTF9_thD_rK3DLON63K1784-0rZkrNqu_rZ56rGK5J0aUSURmgrnwHmdtK8sbTC-jAHiDQf0g'
+                'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODc0ODA1NzUsInVzZXJJRCI6IjVlOWQ5NjI3NjNjNjI5MDIzMzlkMDFhMyIsImFkbWluIjp0cnVlfQ.VV4tFqQog7VyfAVqfWF26V0hii3p1-0Jk_2FfPJcc2GkeSyVKJ9wE2aiSOhkVjGqwMQ246MsmASdBoTltqItW5jaoy3oyUYURW5RpnQQxB_BytNHNlB-6F7W-smyeXIvhl80lztguGmUt1IWQDw5mteMBmt8PtKQ2qZMz6umGKfkBE_D_k-VFfIDdBet6SEszjPi3O2cNicFXeHyOQZcp5d7SgqT13bsJAgcLr2Ogx2R0kCYea56kilObpKFyaX3JtiWiGdOI9YqP_KtCyX2VZlnZVqhwH6VXdOthwozUacI619dMPnsdVq73D-O11iDqtPXEMX9TY6VAAOfVYbLug'
             },
-            body: JSON.stringify(state)
-        }))
-        console.log(`category: ${state.name}`)
+            body: '{"name" : "' + state.category + '"}'
+        })
+        data.data = [...data.data, state.category]
         setState(defaultState)
     }
 
@@ -32,41 +25,22 @@ function InputCategories() {
         setState({ ...state, [event.target.name]: event.target.value })
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Category:</label>
-                <input name="name" value={state.name} type="text" onChange={handleChange} />
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-    )
-}
-
-function GetCategories() {
-    const { data, error } = useSWR('http://localhost:8080/api/v1/categories', fetcher)
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
-    return <DisplayCategories data={data} />
-}
-
-function DisplayCategories({ data }) {
     return (
         <div>
-            <ol>
-                {data.data.map(item => (
-                    <li key={item}>{item}</li>
-                ))}
-            </ol>
-        </div>
-    )
-}
-
-export default function Categories() {
-    return (
-        <div>
-            <PostCategories />
-            <GetCategories />
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="category">Category:</label>
+                <input name="category" id="category" value={state.category} type="text" onChange={handleChange} />
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                <ol>
+                    {data.data.map(item => (
+                        <li key={item}>{item}</li>
+                    ))}
+                </ol>
+            </div>
         </div>
     )
 }
